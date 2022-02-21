193dcd483448da8161aae7567127ddf51f0fd8d7
Armstrong <94338519+ArmstrongGuan@users.noreply.github.com>
2022-02-21 16:37:25 -0500

    Time: 67 ms (11.16%), Space: 13.9 MB (87.05%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -4,8 +4,6 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         loc=[0,0,0] #location of the first 0, 1, 2        
-        print(type(loc))
-        print(loc)
         for i in range(len(nums)):
             if nums[i]==1:
                 nums[i], nums[loc[2]]=nums[loc[2]],nums[i]

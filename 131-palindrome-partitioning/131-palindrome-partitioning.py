e7e89305b84702a6ec94485287f6a33e17fd2e69
Armstrong <94338519+ArmstrongGuan@users.noreply.github.com>
2022-06-21 23:30:59 -0400

    Time: 874 ms (55.69%), Space: 38 MB (5.05%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -2,14 +2,19 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         if s=="":
             return [[]]
-        if len(s)==1:
-            return [[s]]
         result=[]
         for i in range(1,len(s)+1):
             if s[:i]==s[i-1::-1]:
                 result=result+ [[s[:i]]+rest for rest in self.partition(s[i:])]
         return result
-            
+         
+        #https://leetcode.com/problems/palindrome-partitioning/discuss/42025/1-liner-Python-Ruby
+        #based on the above solution:
+        #def partition(self, s):
+        #    return [[s[:i]] + rest
+        #            for i in xrange(1, len(s)+1)
+        #            if s[:i] == s[i-1::-1]
+        #            for rest in self.partition(s[i:])] or [[]]
                 
 
                 

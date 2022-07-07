4d37a22bc2ed021a240ed6c4c7f58dd368bc5ad4
Armstrong <94338519+ArmstrongGuan@users.noreply.github.com>
2022-07-06 20:37:24 -0400

    Time: 6485 ms (15.18%), Space: 14.2 MB (83.02%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -2,6 +2,7 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         #excellent explanation in solution
         #note that approach 2 is collecting a sequence for the smallest end for all sequences of length i
+        #the following is approach 1
         result=[]
         count=0
         for i in range(len(nums)):

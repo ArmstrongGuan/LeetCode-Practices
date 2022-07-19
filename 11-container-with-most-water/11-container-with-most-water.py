894b09480b074eb7813d7f2de7f48a2c682ee347
Armstrong <94338519+ArmstrongGuan@users.noreply.github.com>
2022-07-19 01:16:46 -0400

    Time: 780 ms (92.31%), Space: 27.6 MB (15.08%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,14 +1,21 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         i,j=0, len(height)-1
+        left, right=height[i], height[j]
         vol=min(height[j],height[i])*(j-i)
         while i<j:
             if height[i]<=height[j]:
-                while i < j and height[i]<=height[j]:
-                    vol=max(vol, min(height[j],height[i])*(j-i))
-                    i+=1                
+                while i < j and height[i]<=height[j]:                    
+                    if height[i]>left:
+                        vol=max(vol, min(height[j],height[i])*(j-i))
+                        left=height[i]
+                    i+=1
+                vol=max(vol, min(height[j],height[i])*(j-i))
             else:
                 while i < j and height[j]<=height[i]:
-                    vol=max(vol, min(height[j],height[i])*(j-i))
-                    j-=1                   
+                    if height[j]>right:
+                        vol=max(vol, min(height[j],height[i])*(j-i))
+                        right=height[j]                    
+                    j-=1   
+                vol=max(vol, min(height[j],height[i])*(j-i))
         return vol

c09579199fcce3da31ac8a097fcf4455387fc996
Armstrong <94338519+ArmstrongGuan@users.noreply.github.com>
2022-07-20 15:38:30 -0400

    Time: 78 ms (74.73%), Space: 14.1 MB (76.68%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:        
+        #based on solution: instead of inserting the first element to different places recursively (which could cause problem that inserting the element in different places for different previous permutations could cause the same new permutation), it is a better idea to append different elements
         if len(nums)==1:
             return [nums]
         nums.sort()

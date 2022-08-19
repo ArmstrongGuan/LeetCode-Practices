6ae92f5d1e3b818a4800178fed0c58c210b2a3bb
Armstrong <94338519+ArmstrongGuan@users.noreply.github.com>
2022-08-18 20:10:36 -0400

    Time: 49 ms (60.46%), Space: 13.9 MB (15.69%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,13 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        loc=[0,0,0] #location of the first 0, 1, 2        
+        loc=[0,0,0] #location of the first 0, 1, 2, possibly the same number if there is no certain number        
         for i in range(len(nums)):
             if nums[i]==1:
-                nums[i], nums[loc[2]]=nums[loc[2]],nums[i]
+                nums[i], nums[loc[2]]=nums[loc[2]],nums[i] #for place with 1, exchange it with the first 2
                 loc[2]+=1
             elif nums[i]==0:
-                nums[i], nums[loc[2]], nums[loc[1]]=nums[loc[2]],nums[loc[1]], nums[i]
+                nums[i], nums[loc[2]], nums[loc[1]]=nums[loc[2]],nums[loc[1]], nums[i]#for place with 0, rotate the first 1, the first 2 and the currect place
                 loc[1]+= 1
                 loc[2]+= 1
         return(nums)
